7542bb9c52e6a14967eb66e3f3f8dc5109e7baad
jakubkrajcovic <j.krajcovic@unsw.edu.au>
2019-04-05 18:18:55 +0530

    Implement auto deploy and sample file

---
+++ aws-upload.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Added ../ for credentials file as this config will be used inside build folder
+    credentials:"../aws-credentials.json",
+    bucketName:"www.managingwithdigitaltechnology.com",
+    patterns:[
+        "*.*",
+        "mbax9154/**/*"
+    ]
+};
