ec4578b3cdd7f62857b1f39d9b456b964a39c6b4
jakubkrajcovic <j.krajcovic@unsw.edu.au>
2019-04-06 10:48:44 +0530

    Test commit

--- email_merge_pr.js
+++ email_merge_pr.js
@@ -9,7 +9,7 @@ const PR_NUMBER = process.env.GITHUB_PR_NUMBER,
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
         }
     };
-
+console.log(options);
 request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         const info = JSON.parse(body);
